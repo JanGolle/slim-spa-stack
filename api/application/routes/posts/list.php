955e933a0e5b69e9b9958b4236fa160b6501f000
Elijah Zakirov <elijah.zakirov@gmail.com>
2018-02-16 17:09:59 +0200

    Update routes with some demo logic

--- api/application/routes/posts/list.php
+++ api/application/routes/posts/list.php
@@ -3,31 +3,42 @@ declare(strict_types=1);
 
 /** @var \Slim\App $app */
 
+use App\Entity\Post;
+use App\Service\EntityManager;
 use Slim\Http\Request;
 use Slim\Http\Response;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 $app->get(
     '/posts',
     function (Request $request, Response $response, array $args) {
+        /** @var ContainerBuilder $this */
+        $postRepository = $this->get(EntityManager::class)->getRepository(Post::class);
+
+        $limit = (int) $request->getParam('limit', 10);
+        $offset = (int) $request->getParam('offset', 0);
+
+        $posts = $postRepository->findBy([], null, $limit, $offset);
+        $totalCount = $postRepository->count([]);
+
         $response = $response->withJson(
             [
-                'data' => [
-                    [
-                        'title' => 'Post #1',
-                        'tag' => 'slim',
-                        'description' => 'Post description',
-                    ],
-                    [
-                        'title' => 'Post #2',
-                        'tag' => 'spa',
-                        'description' => 'Post description',
-                    ],
-                ],
+                'data' => array_map(
+                    function (Post $post) {
+                        return [
+                            'id' => $post->getId(),
+                            'title' => $post->getTitle(),
+                            'author' => $post->getAuthor()->getEmail(),
+                            'createdAt' => $post->getCreatedAt()->format('Y-m-d H:i:s'),
+                        ];
+                    },
+                    $posts
+                ),
                 'meta' => [
                     'pagination' => [
-                        'limit' => 10,
-                        'offset' => 0,
-                        'total' => 2,
+                        'limit' => $limit,
+                        'offset' => $offset,
+                        'total' => $totalCount,
                     ],
                 ],
                 'success' => true,
--- api/application/routes/posts/view.php
+++ api/application/routes/posts/view.php
@@ -3,19 +3,28 @@ declare(strict_types=1);
 
 /** @var \Slim\App $app */
 
+use App\Entity\Post;
+use App\Service\EntityManager;
 use Slim\Http\Request;
 use Slim\Http\Response;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 $app->get(
     '/posts/{id:\d+}',
     function (Request $request, Response $response, array $args) {
+        /** @var ContainerBuilder $this */
+        $postRepository = $this->get(EntityManager::class)->getRepository(Post::class);
+        $post = $postRepository->findOneBy(['id' => (int) $args['id']]);
+
         $response = $response->withJson(
             [
-                'data' => [
-                    'title' => 'Post #' . $args['id'],
-                    'tag' => 'slim',
-                    'description' => 'Post description',
-                ],
+                'data' => !is_null($post)
+                    ? [
+                        'id' => $post->getId(),
+                        'title' => $post->getTitle(),
+                        'author' => $post->getAuthor()->getEmail(),
+                        'createdAt' => $post->getCreatedAt()->format('Y-m-d H:i:s'),
+                    ] : [],
                 'meta' => [
                 ],
                 'success' => true,
